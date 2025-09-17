81d256c22139cc967d02d34d8a3ac86a5079c6a4
monboga <monroygabriel9@gmail.com>
2025-09-16 21:12:26 -0600

    feat: poblamos desde el PosApplication con los datos que se requieren

--- src/main/java/com/gabriel/pos_system/repository/ImpuestoSatRepository.java
+++ src/main/java/com/gabriel/pos_system/repository/ImpuestoSatRepository.java
@@ -3,7 +3,9 @@ package com.gabriel.pos_system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gabriel.pos_system.model.ImpuestoSat;
+
 @Repository
-public interface ImpuestoSat extends JpaRepository<ImpuestoSat, String> {
+public interface ImpuestoSatRepository extends JpaRepository<ImpuestoSat, String> {
 
 }

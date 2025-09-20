8027cba13d9825a78b5a50348c3241cab0871adf
monboga <monroygabriel9@gmail.com>
2025-09-19 20:08:08 -0600

    feat: nueva consulta en repositorio de cliente para solo clientes activos

--- src/main/java/com/gabriel/pos_system/repository/ClientRepository.java
+++ src/main/java/com/gabriel/pos_system/repository/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.gabriel.pos_system.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gabriel.pos_system.model.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+    List<Client> findByActivo(Integer activo);
 }

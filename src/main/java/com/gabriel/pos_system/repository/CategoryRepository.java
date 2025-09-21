8fa3f8e1201542bc94c30e18d35503bb9333170e
monboga <monroygabriel9@gmail.com>
2025-09-20 20:16:19 -0600

    fix: paginacion implementada y filtrado de categorias

--- src/main/java/com/gabriel/pos_system/repository/CategoryRepository.java
+++ src/main/java/com/gabriel/pos_system/repository/CategoryRepository.java
@@ -2,7 +2,10 @@ package com.gabriel.pos_system.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gabriel.pos_system.model.Category;
@@ -10,4 +13,9 @@ import com.gabriel.pos_system.model.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByEstado(Integer estado);
+
+    @Query("SELECT c FROM Category c WHERE LOWER(c.descripcion) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    Page<Category> findBySearchTerm(String searchTerm, Pageable pageable);
+
+    Page<Category> findAll(Pageable pageable);
 }
